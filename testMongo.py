4265419827e2a8a839a0737f0ad6d33714c9d80f
Naga <thanhdatbkdn14@gmail.com>
2019-12-15 08:52:38 +0700

    add testMongo

--- testMongo.py
+++ testMongo.py
@@ -2,5 +2,9 @@ import pymongo
 client = pymongo.MongoClient('mongodb://6c89938c-6b23-4f73-9c2b-7b3d390e2dec:DoAHSLsWsGgyim71Ke8G0D2Nb@40.83.74.54:27017/32580e61-969a-4b67-b31d-db0faa896b23')
 db = client['32580e61-969a-4b67-b31d-db0faa896b23']
 cursor = db.temp.find()
-for e in cursor:
+#for e in cursor:
+#    print e
+
+names = db.collection_names()
+for e in names:
     print e
